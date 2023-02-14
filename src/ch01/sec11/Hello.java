63703bb5e551c3be437f21c412fc72d16bc9f14a
mut-jin <dkssud2422@naver.com>
2023-02-14 10:54:37 +0900

    first commit

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,27 @@
+package ch01.sec11;
+/** 
+ * @author 장진명
+ */
+ /*
+장제목: 1장 자바 시작하기
+작성일: 2023.02.03
+*/
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 한줄 주석
+		/*
+		 * 여러줄 주석
+		 * 여러줄 주석
+		 * 여러줄 주석
+		 */
+		// 단축키 : ctrl + /
+		
+//		여러줄
+//		여러줄
+//		여러줄
+		
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}
+}
