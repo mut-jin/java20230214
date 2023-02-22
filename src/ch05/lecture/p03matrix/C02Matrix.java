1a9a54ff0d78d2fbf93cacf1d867a8e605cf4f8d
mut-jin <dkssud2422@naver.com>
2023-02-22 18:09:28 +0900

    20230222

---
+++ src/ch05/lecture/p03matrix/C02Matrix.java
@@ -0,0 +1,35 @@
+package ch05.lecture.p03matrix;
+
+public class C02Matrix {
+	public static void main(String[] args) {
+		int[][] mat = new int[3][4];
+		
+		System.out.println(mat.length); // 3행
+		System.out.println(mat[0].length);
+		System.out.println(mat[1].length);
+		System.out.println(mat[2].length);
+		System.out.println(mat[3].length);
+		
+		for(int i = 0; i<mat.length; i++) {
+			for(int j = 0; j < mat[i].length; j++) {
+				System.out.println(mat[i][j]);
+			}
+		}
+		
+		//각 원소에 값 할당
+		mat[0][0] = 3;
+		mat[0][1] = 5;
+		mat[0][2] = 7;
+		mat[0][3] = 9;
+		
+		// ......
+		mat[1][2] = 10;
+		mat[2][2] = 20;
+		
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat[i].length; j++) {
+				System.out.println(mat[i][j]);
+			}
+		}
+	}
+}
