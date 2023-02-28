6d7a6eeeb9e197c5f2ac1482b560030305551f4f
mut-jin <dkssud2422@naver.com>
2023-02-28 17:11:55 +0900

    20230228

---
+++ src/ch06/exercise/exam17/Printer.java
@@ -0,0 +1,19 @@
+package ch06.exercise.exam17;
+
+public class Printer {
+	public static void println (int a) {
+		System.out.println(a);
+	}
+	
+	public static void println (boolean a) {
+		System.out.println(a);
+	}
+	
+	public static void println (double a) {
+		System.out.println(a);
+	}
+	
+	public static void println (String a) {
+		System.out.println(a);
+	}
+}
